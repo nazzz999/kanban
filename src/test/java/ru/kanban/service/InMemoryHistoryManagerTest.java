1f423575abd286e37fcecb11c2c6cacaed44f5d0
nazarnass <nazarychevaanastacia@yandex.ru>
2025-09-20 21:43:19 +0300

    test()

--- src/test/java/ru/kanban/service/InMemoryHistoryManagerTest.java
+++ src/test/java/ru/kanban/service/InMemoryHistoryManagerTest.java
@@ -1,12 +1,5 @@
 package ru.kanban.service;
 
-import org.junit.jupiter.api.Test;
-import ru.kanban.model.Task;
-import ru.kanban.model.TaskStatus;
-import ru.kanban.model.TaskType;
-
-import static org.assertj.core.api.Assertions.*;
-
 class InMemoryHistoryManagerTest {
 
 }
