30babe83b45c7a5fbde354f31315761c52e74b88
nazarnass <nazarychevaanastacia@yandex.ru>
2025-11-10 21:57:23 +0300

    31.12 Kanban part 5

--- src/main/java/ru/kanban/service/Node.java
+++ src/main/java/ru/kanban/service/Node.java
@@ -1,15 +1,12 @@
 package ru.kanban.service;
 
-import ru.kanban.model.Task;
-
 public class Node<T> {
+
     T task;
     Node<T> next;
     Node<T> prev;
 
-    public Node (T task) {
+    public Node(T task) {
         this.task = task;
     }
-
-
 }
